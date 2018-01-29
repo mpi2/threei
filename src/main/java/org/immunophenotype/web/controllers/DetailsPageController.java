bf0d969af84d8d916215dd5273920da65a2f85ae
jwgwarren <warren.jonathan@gmail.com>
2018-01-29 16:29:15 +0000

    removed old jquery and fancybox opens charts in new ones

--- src/main/java/org/immunophenotype/web/controllers/DetailsPageController.java
+++ src/main/java/org/immunophenotype/web/controllers/DetailsPageController.java
@@ -1,16 +1,14 @@
 package org.immunophenotype.web.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.immunophenotype.web.services.DetailsService;
 import org.immunophenotype.web.services.ParameterDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Controller
 public class DetailsPageController {
@@ -42,5 +40,65 @@ public class DetailsPageController {
         return "procedure";
 
     }
+    
+    
+//    @RequestMapping(value = "/chart", produces = "text/html;charset=UTF-8")
+//    @ResponseBody
+//    public void chartPage(HttpServletRequest request,  HttpServletResponse response) {
+//
+//
+////	String paramValue = "param\\with\\backslash";
+////	String yourURLStr = "http://host.com?param=" + java.net.URLEncoder.encode(paramValue, "UTF-8");
+////	java.net.URL url = new java.net.URL(yourURLStr);
+//	try {
+//		String queryString=request.getQueryString();
+//		String urlString = "https://www.mousephenotype.org/data/charts?phenotyping_center=WTSI&bare=true&accession=MGI:894679&parameter_stable_id=SLM_SLM_016_001";//+queryString;//request.getParameter("url");
+//		//response.setHeader("Content-Type", "text/plain");
+//		// response.setContentType(“text/xml”);
+//		urlString = urlString.replace(" ", "" + "");
+//		System.out.println("urlString="+urlString);
+//		URLConnection connection = new URL(urlString).openConnection();
+//		
+//		//String contentEncoding = connection.getHeaderField("Content-Encoding");// should
+//																				// be”Content-Encoding”,”ISO-8859-1″
+//		// if (contentType.startsWith(“text”)) {
+//		// String charset = “ISO-8859-1”;//this encoding is what the registry
+//		// uses so must be set here to override default;
+//		// System.out.println(“charset=”+charset);
+//		response.setHeader("Access-Control-Allow-Origin", "*");
+//		BufferedReader reader = null;
+//		response.setContentType("text/html;charset=utf-8");
+//		PrintWriter writer = response.getWriter();
+//		
+//		try {
+//			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//			for (String line; (line = reader.readLine()) != null;) {
+//				
+//				writer.println(line);
+//				System.out.println(line);
+//			}
+//		} finally {
+//			if (reader != null)
+//				try {
+//					reader.close();
+//				} catch (IOException logOrIgnore) {
+//				}
+//		}
+//
+//		reader.close();
+//		writer.flush();
+//		writer.close();
+//	} catch (MalformedURLException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	} catch (IOException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//	
+//
+//	}
+
+
 
 }
